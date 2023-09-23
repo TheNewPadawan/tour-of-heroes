c5b76fe99c71c541f5a2cb70bb6ff8b0beb64738
TheNewPadawan <lambertofulgione1@gmail.com>
2023-09-23 12:37:49 +0200

    user constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  title: string = 'tour-of';
+
+  constructor() {
+    this.title += '-heroes';
+  }
 }
