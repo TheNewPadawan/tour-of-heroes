86b596a2dbf9684c916fa323020a307a8238c8f2
TheNewPadawan <lambertofulgione1@gmail.com>
2023-10-04 20:08:18 +0200

    Display a list of heroes implemented

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,4 +9,9 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
   heroes: Hero[] = HEROES;
+  selectedHero?: Hero; // === selectedHero: Hero | undefined
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
