4a71b14c4e91df25b3f1f5e454f741ad1f5910ba
TheNewPadawan <lambertofulgione1@gmail.com>
2023-10-25 18:41:56 +0200

    Retrieve HEROES/HERO from fake API/DB

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service'
 import { HEROES } from './mock-heroes';
@@ -8,22 +10,41 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl: string = 'api/heroes';
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+          private http: HttpClient, 
+          private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes: Observable<Hero[]> = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    const heroes: Observable<Hero[]> =
+            this.http.get<Hero[]>(this.heroesUrl).pipe(
+                  tap(_ => this.log('fetched heroes')),
+                  catchError(this.handleError<Hero[]>(
+                          'getHeroes', [])));
     return heroes;
   }
   
   getHero(id: number): Observable<Hero | undefined> {
-    const hero: Hero | undefined = HEROES.find(h => h.id === id);
-    if (hero === undefined) {
-      this.messageService.add('HeroService: hero not found');
-    } else {
-      this.messageService.add(`HeroService: fetched hero id=${id}`);
-    }
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+            tap(_ => this.log(`fetched hero id=${id}`)),
+            catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
